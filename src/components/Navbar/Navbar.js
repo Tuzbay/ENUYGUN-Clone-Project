99a51be9e35a7aa79a062149e051eacd51231948
Tuzbay <t.uzbayyelce@gmail.com>
2023-03-09 15:22:04 +0300

    SignUp Popup Added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,9 +8,11 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import CloseIcon from "@mui/icons-material/Close";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
 function Navbar() {
   const [loginPopup, setLoginPopup] = useState(false);
+  const [sigUpPopup, setSignUpPopup] = useState(false);
   const [passwordType, setPasswordType] = useState("Password");
   const [passwordInput, setPasswordInput] = useState("");
 
@@ -60,7 +62,14 @@ function Navbar() {
           >
             Giriş Yap
           </li>
-          <li className="navbar__right-list-item hover">Üye Ol</li>
+          <li
+            className="navbar__right-list-item hover"
+            onClick={() => {
+              setSignUpPopup(true);
+            }}
+          >
+            Üye Ol
+          </li>
         </ul>
       </div>
 
@@ -110,6 +119,140 @@ function Navbar() {
               setLoginPopup(false);
             }}
           />
+        </div>
+      )}
+
+      {sigUpPopup && (
+        <div className="signUp">
+          <div className="signUp__left">
+            <img
+              src="https://cdn2.enuygun.com/media/lib/uploads/image/logo-kaynagi-33816.jpeg"
+              alt="ENUYGUN"
+            />
+            <p>Tüm bilgileriniz elinizin altında!</p>
+            <ul className="signUp__left-list">
+              <li className="signUp__left-list-item">
+                <div>
+                  <CheckCircleOutlineIcon />
+                </div>
+                <div>
+                  <strong>Yolcu bilgilerinizi kaydedin</strong>
+                  <p>
+                    Kendinize ve sevdiklerinize ait bilgileri kaydedin, bilet ve
+                    rezervasyon işlemlerinizi çok daha hızlı halledin.
+                  </p>
+                </div>
+              </li>
+              <li className="signUp__left-list-item">
+                <div>
+                  <CheckCircleOutlineIcon />
+                </div>
+                <div>
+                  <strong>Ödemenizi kolayca yapın</strong>
+                  <p>
+                    Kredi kartı bilgilerinizi güvenle saklayın ve ödemenizi
+                    sadece saniyeler içinde yapın.
+                  </p>
+                </div>
+              </li>
+              <li className="signUp__left-list-item">
+                <div>
+                  <CheckCircleOutlineIcon />
+                </div>
+                <div>
+                  <strong>Tüm detaylar tek bir yerde</strong>
+                  <p>
+                    Rezervasyonlarınıza ve bilet bilgilerinize dilediğiniz zaman
+                    ulaşın.
+                  </p>
+                </div>
+              </li>
+              <li className="signUp__left-list-item">
+                <div>
+                  <CheckCircleOutlineIcon />
+                </div>
+                <div className="signUp__left-list-item-desc">
+                  <strong>Kredi kartı puanlarınızı kullanın</strong>
+                  <p>
+                    Kredi kartı puanlarınızı hesabınıza aktarın, biriken
+                    puanlarla uçak ya da otobüs biletinizi alın, otel
+                    rezervasyonunuzu yapın.
+                  </p>
+                </div>
+              </li>
+            </ul>
+            <div className="signUp__left-bottom">
+              <div>
+                <p>
+                  Profesyonellere özel kurumsal üyelik avantajlarından
+                  yararlanın...
+                </p>
+              </div>
+              <div className="signUpCoorp">
+                <button>Kurumsal Üye Ol</button>
+              </div>
+            </div>
+          </div>
+          <div className="signUp__right">
+            <h2>Enuygun'a Üye Ol</h2>
+            <input type="email" placeholder="E-Posta" />
+            <div className="passwordInput">
+              <input
+                type={passwordType}
+                onChange={handlePasswordChange}
+                value={passwordInput}
+                placeholder="Şifre"
+              />
+              {passwordType === "password" ? (
+                <VisibilityIcon
+                  className="passwordIcon signUpPassIcon"
+                  onClick={togglePassword}
+                />
+              ) : (
+                <VisibilityOffIcon
+                  className="passwordIcon signUpPassIcon"
+                  onClick={togglePassword}
+                />
+              )}
+            </div>
+            <div className="checkboxPart">
+              <input className="chechbox" type="checkbox" />
+              <span>
+                İndirimler ve kampanyalardan <strong>Rıza Metni</strong> Rıza
+                kapsamında haberdar olmak istiyorum.
+              </span>
+            </div>
+            <button className="signUpPopup">Üye Ol</button>
+            <div className="logInPopup SignUpPopup">
+              <div className="facebook facebook-sign">
+                <FacebookIcon />
+                Facebook ile üye ol
+              </div>
+              <div className="google google-sign">
+                <GoogleIcon />
+                Google ile üye ol
+              </div>
+            </div>
+            <div className="signUp__bottom-desc">
+              <p>
+                Kişisel verileriniz <strong>Aydınlatma Metni</strong> kapsamında
+                işleniyor. Üye olarak{" "}
+                <strong>Enuygun Kullanım Koşulları</strong>
+                'nı kabul ettiğinizi onaylamış olursunuz.
+              </p>
+            </div>
+            <div className="bottomIf">
+              <p>
+                Üyeliğin varsa <strong>Giriş Yap</strong>
+              </p>
+            </div>
+            <CloseIcon
+              className="closeIcon"
+              onClick={() => {
+                setSignUpPopup(false);
+              }}
+            />
+          </div>
         </div>
       )}
     </div>
