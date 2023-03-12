c85025e5681acc733db20e5719bd4edcd5cd27c0
Tuzbay <t.uzbayyelce@gmail.com>
2023-03-12 17:20:45 +0300

    Info and Image Responsive Added

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -5,15 +5,17 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 function Info() {
   return (
     <div className="info">
-      <div className="info__left">
-        <h1>Türkiye'nin Seyahat Sitesi</h1>
-        <div className="count">17.000.000+</div>
-        <div className="visiter">Aylık Ziyaretçi</div>
-      </div>
-      <div className="info__middle">
-        <img src="/images/call-center-new.png" alt="" />
-        <img src="/images/ticket-cancel-new.png" alt="" />
-        <img src="/images/price-alert-new.png" alt="" />
+      <div className="deneme">
+        <div className="info__left">
+          <h1>Türkiye'nin Seyahat Sitesi</h1>
+          <div className="count">17.000.000+</div>
+          <div className="visiter">Aylık Ziyaretçi</div>
+        </div>
+        <div className="info__middle">
+          <img src="/images/call-center-new.png" alt="" />
+          <img src="/images/ticket-cancel-new.png" alt="" />
+          <img src="/images/price-alert-new.png" alt="" />
+        </div>
       </div>
       <div className="info__right">
         <p className="p-gray">KLİŞE HEDİYELERDEN SIKILANLARA</p>
